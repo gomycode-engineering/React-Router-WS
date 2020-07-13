721592718a7e8f643cfdb8f0a25df6ecc8175a63
anis-meliti <ansmeliti@gmail.com>
2020-07-13 16:24:25 +0100

    implement the solution

---
+++ src/Components/AdminArea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminArea = () => {
+  return (
+    <div>
+      <h1>Hello from admin</h1>
+    </div>
+  );
+};
+
+export default AdminArea;
---
+++ src/Components/Category.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Category = () => {
+  return (
+    <div>
+      <h1>Hello from Category</h1>
+    </div>
+  );
+};
+
+export default Category;
